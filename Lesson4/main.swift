8eb711ec083d40c8bc1a52e2f6a66b17bacae585
RayenD <zufirka@icloud.com>
2021-01-30 05:32:38 +0300

    truck

--- Lesson4/main.swift
+++ Lesson4/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+enum MyCar {
+    case foo
+}
 
+struct Car {
+    let max : Double
+    var truck: Double
+    
+    
+    init(max: Double) {
+        self.max = max
+        self.truck = 0
+    }
+    
+    mutating func load(cargo: Double){
+        if (truck + cargo) <= max {
+            truck += cargo
+            print("remain: \(max - truck) ")
+        } else {
+            print("too much \(cargo), remained: \(max - truck)")
+        }
+        
+    }
+}
+
+var car = Car(max: 100)
+car.load(cargo: 50)
+car.load(cargo: 60)
